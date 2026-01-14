e98f7cf6a825a0d3a7f6cf7361daad476d1bd080
Petrik Diak <diak@petrik.hu>
2026-01-14 10:39:20 +0100

    majdnem jó

--- FlappyBird/MainWindow.xaml.cs
+++ FlappyBird/MainWindow.xaml.cs
@@ -8,6 +8,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Linq;
+using System;
 
 using System.Windows.Threading;
 
@@ -26,19 +28,108 @@ namespace FlappyBird
         bool gameOver;
         Rect falappybirdHitbox;
 
-        public MainWindow()
+		public DispatcherTimer MainEventTimer { get; }
+
+		public MainWindow()
         {
             InitializeComponent();
+
+			gameTimer.Tick += MainEventTimer;
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+            StartGame();
         }
+
+		private void MainEventTimer(object sender, EventArgs e)
+        {
+			falappybirdHitbox = new Rect(
+			   Canvas.GetLeft(madar),
+			   Canvas.GetTop(madar),
+			   madar.Width,
+			   madar.Height);
+
+			Canvas.SetTop(madar, Canvas.GetTop(madar) + gravity);
+
+			foreach (var x in MyCanvas.Children.OfType<Image>())
+			{
+				if ((string)x.Tag == "obs1")
+				{
+					Canvas.SetLeft(x, Canvas.GetLeft(x) - 5);
+
+					Rect pipeHitBox = new Rect(
+						Canvas.GetLeft(x),
+						Canvas.GetTop(x),
+						x.Width,
+						x.Height);
+
+					if (falappybirdHitbox.IntersectsWith(pipeHitBox))
+					{
+						EndGame();
+					}
+
+					if (Canvas.GetLeft(x) < -100)
+					{
+						Canvas.SetLeft(x, 800);
+						score++;
+						lbl_Score.Content = "Score: " + score;
+					}
+				}
+			}
+
+			if (Canvas.GetTop(madar) < 0 || Canvas.GetTop(madar) > 450)
+			{
+				EndGame();
+			}
+		}
 
 		private void KeyIsDown(object sender, KeyEventArgs e)
 		{
+			if (e.Key == Key.Space && !gameOver)
+			{
+				gravity = -8;
+			}
 
+			if (e.Key == Key.R && gameOver)
+			{
+				StartGame();
+			}
 		}
 
 		private void KeyIsUp(object sender, KeyEventArgs e)
 		{
-
+			if (e.Key == Key.Space)
+			{
+				gravity = 8;
+			}
 		}
+
+        private void StartGame()
+        {
+			MyCanvas.Focus();
+			score = 0;
+			gameOver = false;
+			lbl_Score.Content = "Score: 0";
+
+			Canvas.SetTop(madar, 190);
+			Canvas.SetLeft(madar, 50);
+
+			foreach (var x in MyCanvas.Children.OfType<Image>())
+			{
+				if ((string)x.Tag == "obs1")
+				{
+					Canvas.SetLeft(x, 600 + new Random().Next(300));
+				}
+			}
+
+			gameTimer.Start();
+		}
+
+        private void EndGame()
+        {
+			gameTimer.Stop();
+			gameOver = true;
+			lbl_Score.Content += "  GAME OVER (R)";
+		}
+        
+
 	}
 }
